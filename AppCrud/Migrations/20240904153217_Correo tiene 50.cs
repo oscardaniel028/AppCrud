3a563d2a2cf2aa597258710f09e8ac5150d7fbdc
Oscar Sanchez <oscardaniel028@gmail.com>
2024-10-07 10:02:03 -0500

    first commit

---
+++ AppCrud/Migrations/20240904153217_Correo tiene 50.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class Correotiene50 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Correo",
+                table: "Empleado",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Correo",
+                table: "Empleado",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ AppCrud/Validations/ValidationEmpleadoServices.cs
@@ -0,0 +1,23 @@
+﻿using AppCrud.Models;
+
+namespace AppCrud.Validations
+{
+    public  static class ValidationEmpleadoServices
+    {
+        public const string const1 = "prueba";
+       public static bool ValidateCreate(this Empleado empleado)
+        {
+            if (empleado == null)
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(empleado.NombreCompleto))
+            {
+                return false;
+            }
+            return true;
+
+
+        }
+    }
+}
